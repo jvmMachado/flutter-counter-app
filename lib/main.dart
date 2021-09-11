114071ee5f1ae1719eae82f10e961b998d93bd8a
jvmmachado <joao.machado@somagrupo.com.br>
2021-09-11 19:47:05 -0300

    add theme, Scaffold and AppBar

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.cyan),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Meu primeiro app'),
+      ),
+      body: Container(
+          color: Colors.purple,
+          child: Center(
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.yellow,
+            ),
+          )),
+    ),
+  ));
+}
